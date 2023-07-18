28b121617433d0757d3ad577b1a4930abeaa1459
Binh <lethanhbinh6203@gmail.com>
2023-07-18 15:22:43 +0700

    Fix landing page metadata 4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head />
       <body className={inter.className}>
         <ToasterProvider />
         <NavBar />
