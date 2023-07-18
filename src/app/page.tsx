93d5abbbf363ad82cbcdc478998beff1961efbb7
Binh <lethanhbinh6203@gmail.com>
2023-07-18 15:26:31 +0700

    Fix landing page metadata 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
     <>
       <nav className="fixed w-full bg-white z-10 h-[85px]"></nav>
 
-      <main className="min-h-screen relative z-10">
+      {/* <main className="min-h-screen relative z-10">
         <div className="h-screen sm:h-[90vh] w-full -skew-y-6 absolute -top-24 bg-gradient-to-tr from-sky-600 to-sky-400 -z-10"></div>
 
         <header className="h-screen max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 p-4">
@@ -40,7 +40,7 @@ export default function Home() {
           <Questions />
           <LastCTA />
         </section>
-      </main>
+      </main> */}
 
       <Footer />
     </>
