d6881b6c978f7836225d13dc5615f9b55007cc9f
Binh <lethanhbinh6203@gmail.com>
2023-07-18 17:34:43 +0700

    Add social card to listing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,18 +6,18 @@ import { Metadata } from 'next'
 
 const inter = Inter({ subsets: ['latin'] })
 
-// export const metadata: Metadata = {
-//   title: 'RentTomo - Ứng dụng tìm trọ, bạn cùng phòng lý tưởng',
-//   description: 'Tìm kiếm trọ hiệu quả dựa trên vị trí, tiện ích phòng và ngân sách của bạn. Hoặc, tham gia ngay vào những phòng đã có thành viên để tiết kiệm chi phí và gặp gỡ những người bạn mới.',
-//   openGraph: {
-//     images: {
-//       url: '/images/logo_full.png',
-//       alt: 'RentTomo Logo',
-//       width: 1200,
-//       height: 630,
-//     },
-//   },
-// }
+export const metadata: Metadata = {
+  title: 'RentTomo - Ứng dụng tìm trọ, bạn cùng phòng lý tưởng',
+  description: 'Tìm kiếm trọ hiệu quả dựa trên vị trí, tiện ích phòng và ngân sách của bạn. Hoặc, tham gia ngay vào những phòng đã có thành viên để tiết kiệm chi phí và gặp gỡ những người bạn mới.',
+  openGraph: {
+    images: {
+      url: '/images/logo_full.png',
+      alt: 'RentTomo Logo',
+      width: 1200,
+      height: 240,
+    },
+  },
+}
 
 export default function RootLayout({
   children,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,21 +6,7 @@ import LastCTA from "@/components/landing/LastCTA";
 import Logo from "@/components/nav/Logo";
 import UserMenu from "@/components/nav/UserMenu";
 import UserMenuFallback from "@/components/nav/UserMenuFallback";
-import { Metadata } from "next";
 import { Suspense } from "react";
-
-export const metadata: Metadata = {
-  title: 'RentTomo - Ứng dụng tìm trọ, bạn cùng phòng lý tưởng',
-  description: 'Tìm kiếm trọ hiệu quả dựa trên vị trí, tiện ích phòng và ngân sách của bạn. Hoặc, tham gia ngay vào những phòng đã có thành viên để tiết kiệm chi phí và gặp gỡ những người bạn mới.',
-  openGraph: {
-    images: {
-      url: '/images/logo_full.png',
-      alt: 'RentTomo Logo',
-      width: 1200,
-      height: 630,
-    },
-  },
-}
 
 export default function Home() {
   return (
