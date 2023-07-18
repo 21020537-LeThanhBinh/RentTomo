849284c90eeefe21d14ff8d8aa16f19d3e832e12
Binh <lethanhbinh6203@gmail.com>
2023-07-18 15:10:18 +0700

    Fix landing page metadata 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,9 @@ import Hero from "@/components/landing/Hero";
 import LastCTA from "@/components/landing/LastCTA";
 import Logo from "@/components/nav/Logo";
 import UserMenu from "@/components/nav/UserMenu";
+import { Metadata } from "next";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'RentTomo - Ứng dụng tìm nơi ở, bạn cùng phòng lý tưởng',
   description: 'Tìm kiếm trọ hiệu quả dựa trên vị trí, tiện ích phòng và ngân sách của bạn. Hoặc, tham gia ngay vào những phòng đã có thành viên để tiết kiệm chi phí và gặp gỡ những người bạn mới.',
   openGraph: {
